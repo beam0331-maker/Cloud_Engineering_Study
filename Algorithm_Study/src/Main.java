0efbd7d92ab4e4b2c83961163ed45e810172c414
beam0331 <beam0331@gmail.com>
2026-04-07 00:13:31 +0900

    doc: 백준 2164번_ver.3 내용: ai가 말해준 가장 빠른 방법인 입력의 2N을 만큼의 배열을 사용한 풀이법. 속도는 가장 빨랐지만 직관적으로 떠오르진 않을것 같다

--- Algorithm_Study/src/Main.java
+++ Algorithm_Study/src/Main.java
@@ -5,15 +5,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayDeque<Integer> deque = new ArrayDeque<>(n);
-        for (int i = 1; i <= n; i++) {
-            deque.add(i);
+        int[] q = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            q[i] = i + 1;
         }
-        while(deque.size() > 1){
-            deque.poll();
-            deque.add(deque.poll());
+        int head = 0;
+        int tail = n;
+
+        // 문제에서 마지막 1장이 남을때 까지 이므로 tail - head은 1이 남을때 까지 진행된다.
+        // tail이 한번 증가할때 head는 두번 증가하므로 진행과정중에 1칸까지 따라잡힌다.
+        while ((tail - head) > 1) {
+            head++;
+            q[tail++] = q[head++];
         }
-        System.out.println(deque.poll());
+
+        System.out.println(q[head]);
     }
 
 }
