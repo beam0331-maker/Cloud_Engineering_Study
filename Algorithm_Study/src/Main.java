7529227c7245049fe1c813918e005be7d5184353
beam0331 <beam0331@gmail.com>
2026-04-08 23:33:02 +0900

    doc: 백준 28279번 기본적인 Deque의 기능 문제

--- Algorithm_Study/src/Main.java
+++ Algorithm_Study/src/Main.java
@@ -4,24 +4,62 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        ArrayDeque<Integer> deque = new ArrayDeque<>(n);
+        int n = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            deque.add(i + 1);
-        }
-        StringJoiner sj = new StringJoiner(", ", "<", ">");
-        while (!deque.isEmpty()) {
-            for (int i = 1; i < k ; i++) {
-                deque.add(deque.poll());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            switch (cmd){
+                case 1 :
+                    deque.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    deque.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case 3:
+                    if(!deque.isEmpty()){
+                        sb.append(deque.pollFirst()).append("\n");
+                    }else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case 4:
+                    if(!deque.isEmpty()){
+                        sb.append(deque.pollLast()).append("\n");
+                    }else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case 5:
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case 6 :
+                    if(deque.isEmpty())
+                        sb.append(1).append("\n");
+                    else
+                        sb.append(0).append("\n");
+                    break;
+                case 7:
+                    if(!deque.isEmpty()){
+                        sb.append(deque.peekFirst()).append("\n");
+                    }else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case 8:
+                    if(!deque.isEmpty()){
+                        sb.append(deque.peekLast()).append("\n");
+                    }else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
             }
-            sj.add(deque.poll().toString());
+
         }
 
-        System.out.println(sj);
+        System.out.println(sb);
+
     }
 
 }
